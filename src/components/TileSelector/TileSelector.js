5d81aeac506d58bf35cead24589d672c501f0b93
Krishna <krishna@adventurebox.com>
2020-06-28 22:28:06 +0200

    Module-2

--- src/components/TileSelector/TileSelector.js
+++ src/components/TileSelector/TileSelector.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './TileSelector.css'
 
-const TileSelector = () => {
+const TileSelector = (props) => {
 
 
   const dropdown = (
@@ -15,7 +15,8 @@ const TileSelector = () => {
  return (
    <div className='tileSelector'>
      <div>Number of Tiles</div>
-     <div className='tileSelectorDropdown'>
+     <div className='tileSelectorDropdown' >
+       {props.numTiles}
        {dropdown}
      </div>
    </div>
