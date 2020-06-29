3ed84db22ac601c6b99af287e362d5b48fb48f5d
Krishna <krishna@adventurebox.com>
2020-06-29 23:10:44 +0200

    Module-3

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -6,9 +6,10 @@ const Tile = (props) => {
 
   const dynamicColor = props.selected || props.matched ? { backgroundColor: props.color} : null
   return (
-    <div className='Tile' style={dynamicColor}>
+    <div className='Tile' onClick={()=> {props.handleTileClicked(props.id, props.color)}} style={dynamicColor}>
        { props.selected || props.matched ? <props.svg /> : null }
     </div>
+    
   )
 }
 
